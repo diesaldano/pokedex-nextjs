4e08cd1827247f67cb7af45a79913fb5b21bc277
Diego Saldano <dsaldano@preminens.com.ar>
2022-05-17 00:39:15 -0300

    feat: add domain img in next.config and add sw package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["http://localhost:3000/", "https://raw.githubusercontent.com/PokeAPI/sprites/master"],
+}
 }
 
 module.exports = nextConfig
